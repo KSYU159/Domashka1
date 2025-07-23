199bd0d19fb047e833068e3121a5f00a770ff50c
KSYU159 <KKB88@mail.ru>
2025-07-24 00:25:42 +0500

    2

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите ваше имя: ");
+        String username = scanner.nextLine();
+        System.out.println("Привет, " + username);
+        scanner.close();
+    }
 }
